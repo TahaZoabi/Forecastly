55bbb0f98619ee58be52e6e32a5e12efdd6fae1a
Taha Zoabi <zoabi03@gmail.com>
2025-01-13 11:24:24 +0200

    API call on search input

--- src/components/Search.vue
+++ src/components/Search.vue
@@ -1,6 +1,9 @@
 <script setup lang="ts">
 import { Input } from "@/components/ui/input";
 import { Search } from "lucide-vue-next";
+import { reactive } from "vue";
+
+const emit = defineEmits(["place-data"]);
 
 const date = new Date().toLocaleString("en-US", {
   weekday: "long",
@@ -8,6 +11,38 @@ const date = new Date().toLocaleString("en-US", {
   day: "numeric",
   year: "numeric",
 });
+
+const searchTerm = reactive({
+  query: "",
+  timeout: null,
+  results: [],
+});
+
+const handleSearch = () => {
+  clearTimeout(searchTerm.timeout);
+  searchTerm.timeout = setTimeout(async () => {
+    if (searchTerm.query !== "") {
+      const res = await fetch(
+        `http://api.weatherapi.com/v1/search.json?key=ab2d06f29c28457e90b73347251301&q=${searchTerm.query}`,
+      );
+
+      const data = await res.json();
+      searchTerm.results = data;
+    } else {
+      searchTerm.results = [];
+    }
+  }, 500);
+};
+
+const getWeather = async (id) => {
+  const res = await fetch(
+    `http://api.weatherapi.com/v1/forecast.json?key=ab2d06f29c28457e90b73347251301&q=id:${id}&days=3&aqi=yes&alerts=no`,
+  );
+  const data = await res.json();
+  emit("place-data", data);
+  searchTerm.results = [];
+  searchTerm.query = "";
+};
 </script>
 
 <template>
@@ -19,6 +54,8 @@ const date = new Date().toLocaleString("en-US", {
         type="text"
         placeholder="Search Cities..."
         class="pl-10"
+        v-model="searchTerm.query"
+        @input="handleSearch"
       />
       <span
         class="absolute start-0 inset-y-0 flex items-center justify-center px-2"
@@ -29,13 +66,15 @@ const date = new Date().toLocaleString("en-US", {
       <!-- Static Results Dropdown -->
       <div
         class="absolute w-full mt-2 bg-white rounded-lg shadow-lg max-h-60 overflow-y-auto z-10"
+        v-if="searchTerm.results.length > 0"
       >
-        <ul>
-          <!--          <li-->
-          <!--            class="px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 cursor-pointer"-->
-          <!--          >-->
-          <!--            New York-->
-          <!--          </li>-->
+        <ul v-for="place in searchTerm.results" :key="place">
+          <li
+            class="px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 cursor-pointer"
+            @click="getWeather(place.id)"
+          >
+            {{ place.name }}
+          </li>
         </ul>
       </div>
     </div>
