e06ee6275499bb3a7851a080a6dea82ee805ea10
Taha Zoabi <zoabi03@gmail.com>
2025-01-18 13:41:24 +0200

    clean up

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,11 @@
 <script setup lang="ts">
-import Logo from "@/components/Logo.vue";
 import Footer from "@/components/Footer.vue";
 import Search from "@/components/Search.vue";
 import WeatherCard from "@/components/WeatherCard.vue";
-
 import { ref } from "vue";
 import type { WeatherData } from "@/lib/weather.ts";
-import ThreeDayForecast from "@/components/ThreeDayForecast.vue";
 import { Button } from "@/components/ui/button";
+import WeatherForecast from "@/components/WeatherForecast.vue";
 
 const places = ref<WeatherData[]>([]);
 
@@ -26,17 +24,24 @@ const addPlace = (data: WeatherData) => {
     <main
       class="flex-grow flex flex-col justify-start items-center gap-4 bg-gray-100"
     >
-      <Logo />
+      <header class="p-4 w-full">
+        <h1 class="font-bold text-4xl text-primary select-none p-2 text-center">
+          Forecastly
+        </h1>
+      </header>
       <Search @place-data="addPlace" />
-
       <Button @click="convertTemp"
         >Convert To {{ fahrenheit ? "Celsius" : "Fahrenheit" }}</Button
       >
       <div class="w-full" v-for="place in places" :key="place.location.name">
         <WeatherCard :place="place" :fahrenheit="fahrenheit" />
-        <ThreeDayForecast :place="place" :fahrenheit="fahrenheit" />
+        <h3 class="text-center text-3xl mt-4 font-semibold">
+          Three Days Forecast
+        </h3>
+        <div v-for="day in place.forecast.forecastday" :key="day.date_epoch">
+          <WeatherForecast :day="day" :place="place" :fahrenheit="fahrenheit" />
+        </div>
       </div>
-
       <Footer />
     </main>
   </div>
--- src/components/Logo.vue
+++
@@ -1,9 +0,0 @@
-<script setup lang="ts"></script>
-
-<template>
-  <header class="p-4 w-full">
-    <h1 class="font-bold text-3xl text-primary select-none p-2 text-center">
-      Forecastly
-    </h1>
-  </header>
-</template>
